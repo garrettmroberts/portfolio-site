48cecb19fbd042829c4f20332c51066795c6b031
Garrett <garrettmroberts@gmail.com>
2024-04-16 11:35:50 -0600

    remove template text

--- util/projects.ts
+++ util/projects.ts
@@ -6,12 +6,6 @@ const Projects = [
     description: 'Are you ever indecisive about what you want to eat? WeGusta makes the choice easier by letting you choose cuisine types using a Tinder-like interface. Based on your input, it will recommend a highly rated local restaurant that fits your criteria!  This app is built using React Native and the Google Maps API.  The animations were challenging to build and it was a fun project to work on.',
     image: weGusta,
     link: 'https://github.com/garrettmroberts/wegusta-app'
-  },
-  {
-    title: 'Title',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam euismod lacinia libero a elementum. Aliquam a sem euismod, ornare nibh ut, efficitur lectus. Mauris accumsan, odio ut convallis mollis, sem nulla sagittis enim, sit amet fringilla enim tellus rutrum dui. Vivamus ut diam cursus, consectetur arcu ac, efficitur nisl.',
-    image: 'https://picsum.photos/300/200',
-    link: '#'
   }
 ];
 
