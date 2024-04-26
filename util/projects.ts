da65f1bb0c9fe101f37fe729e93dc30a984ffcf2
Garrett <garrettmroberts@gmail.com>
2024-04-26 16:20:49 -0600

    added another project

--- util/projects.ts
+++ util/projects.ts
@@ -1,4 +1,5 @@
 import weGusta from '../assets/wegusta.png';
+import jsonFormatter from '../assets/json-formatter.png';
 
 const Projects = [
   {
@@ -6,6 +7,12 @@ const Projects = [
     description: 'Are you ever indecisive about what you want to eat? WeGusta makes the choice easier by letting you choose cuisine types using a Tinder-like interface. Based on your input, it will recommend a highly rated local restaurant that fits your criteria!  This app is built using React Native and the Google Maps API.  The animations were challenging to build and it was a fun project to work on.',
     image: weGusta,
     link: 'https://github.com/garrettmroberts/wegusta-app'
+  },
+  {
+    title: 'Json Formatter',
+    description: 'Json formatter is a tool that allows a user to upload a json file, reorganize it, and download the updated file.  It was built using React, Typescript, and Express.',
+    image: jsonFormatter,
+    link: 'https://github.com/garrettmroberts/json-formatter'
   }
 ];
 
