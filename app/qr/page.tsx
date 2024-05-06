57509b334e035ff5030f79d3c60e3f0daee5e852
Garrett <garrettmroberts@gmail.com>
2024-05-06 10:24:14 -0600

    twaek qr code

--- app/qr/page.tsx
+++ app/qr/page.tsx
@@ -1,34 +1,14 @@
 "use client"
 import {QRCodeSVG} from 'qrcode.react';
 import styles from './page.module.css';
-import { useState, useEffect } from 'react';
 
-function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height
-  };
-}
 
 const QRDisplay = () => {
-    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-
-    useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
-    }
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-
     return (
         <div className={styles.wrapper}>
             <QRCodeSVG
                 value={"https://garrettroberts.dev/"}
-                size={Math.min(windowDimensions.width * 0.8, 400)}
+                size={250}
                 bgColor={"#385170"}
                 fgColor={"#ececec"}
                 level={"L"}
@@ -37,9 +17,9 @@ const QRDisplay = () => {
                     src: "./icon.png",
                     x: undefined,
                     y: undefined,
-                    height: Math.min(windowDimensions.width * 0.1, 100),
-                    width: Math.min(windowDimensions.width * 0.1, 100),
-                    excavate: true,
+                    height: 40,
+                    width: 40,
+                    excavate: true
                 }}
             />
         </div>
