597d5ab4da0f38ee072bc649452d30584807df05
Garrett Roberts <garrettmroberts@gmail.com>
2025-04-04 12:02:53 -0600

    added pocket-tutor project

--- util/projects.ts
+++ util/projects.ts
@@ -1,19 +1,21 @@
-import weGusta from '../assets/wegusta.png';
-import jsonFormatter from '../assets/json-formatter.png';
+import weGusta from "../assets/wegusta.png";
+import pocketTutor from "../assets/pockettutor.png";
 
 const Projects = [
   {
-    title: 'WeGusta',
-    description: 'Are you ever indecisive about what you want to eat? WeGusta makes the choice easier by letting you choose cuisine types using a Tinder-like interface. Based on your input, it will recommend a highly rated local restaurant that fits your criteria!  This app is built using React Native and the Google Maps API.  The animations were challenging to build and it was a fun project to work on.',
+    title: "WeGusta",
+    description:
+      "Are you ever indecisive about what you want to eat? WeGusta makes the choice easier by letting you choose cuisine types using a Tinder-like interface. Based on your input, it will recommend a highly rated local restaurant that fits your criteria!  This app is built using React Native and the Google Maps API.  The animations were challenging to build and it was a fun project to work on.",
     image: weGusta,
-    link: 'https://github.com/garrettmroberts/wegusta-app'
+    link: "https://github.com/garrettmroberts/wegusta-app",
   },
   {
-    title: 'Json Formatter',
-    description: 'Json formatter is a tool that allows a user to upload a json file, reorganize it, and download the updated file.  It was built using React, Typescript, and Express.',
-    image: jsonFormatter,
-    link: 'https://github.com/garrettmroberts/json-formatter'
-  }
+    title: "PocketTutor",
+    description:
+      "Learning on the go! PocketTutor will create a series of audio lessons for you based on any subject you choose.  This project was built using NextJS and OpenAI.",
+    image: pocketTutor,
+    link: "https://github.com/garrettmroberts/one-app-a-week/tree/main/pocket-tutor",
+  },
 ];
 
 export default Projects;
